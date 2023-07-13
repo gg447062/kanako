076ff79bb768d270b9bc6d03ecc98fa6dcbc56a3
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2023-07-13 14:40:00 -0600

    adds temporary button to show/hide overlay for feedback

--- src/app/page.js
+++ src/app/page.js
@@ -1,49 +1,73 @@
-import Canvas from '@/components/Canvas';
+'use client';
 import styles from './styles/home.module.css';
-import Image from 'next/image';
+import Canvas from '@/components/Canvas';
+import Overlay from '@/components/Overlay';
+import { useState } from 'react';
 
 export default function Home() {
+  const [showOverlay, setShowOverlay] = useState(true);
+  function toggleOverlay() {
+    setShowOverlay(!showOverlay);
+  }
   return (
     <article className={styles.article}>
-      <video
-        className={styles.videos}
-        src={'/chantal.mp4'}
-        muted
-        autoPlay
-        loop
-        width={1200}
-        height={800}
-      ></video>
-      <video
-        className={styles.videos}
-        src={'voyeur.mp4'}
-        muted
-        autoPlay
-        loop
-        width={1200}
-        height={800}
-      ></video>
-      <Image
-        className={styles.imageOne}
-        src="/topp.png"
-        alt="abstract-shape"
-        width={1200}
-        height={600}
-      ></Image>
-      <Image
-        className={styles.imageTwo}
-        src="/_middle.png"
-        alt="abstract-shape"
-        width={1200}
-        height={600}
-      ></Image>
-      <Image
-        className={styles.imageThree}
-        src="/_bottom.png"
-        alt="abstract-shape"
-        width={1200}
-        height={600}
-      ></Image>
+      <button className={styles.button} onClick={toggleOverlay}>
+        {showOverlay ? 'hide' : 'show'}
+      </button>
+      {!showOverlay && (
+        <div className={styles.article}>
+          <video
+            className={styles.videos}
+            src={
+              'https://res.cloudinary.com/dletnd7ha/video/upload/v1689270479/kanako-site/chantal_fihnbe.mp4'
+            }
+            muted
+            autoPlay
+            loop
+            width={1200}
+            height={800}
+          ></video>
+          <video
+            className={styles.videos}
+            src={
+              'https://res.cloudinary.com/dletnd7ha/video/upload/v1689270498/kanako-site/voyeur_aa5wgm.mp4'
+            }
+            muted
+            autoPlay
+            loop
+            width={1200}
+            height={800}
+          ></video>
+        </div>
+      )}
+      {showOverlay && (
+        <div>
+          <video
+            className={styles.videosAbsolute}
+            src={
+              'https://res.cloudinary.com/dletnd7ha/video/upload/v1689270479/kanako-site/chantal_fihnbe.mp4'
+            }
+            muted
+            autoPlay
+            loop
+            width={1200}
+            height={800}
+          ></video>
+          <video
+            className={styles.videosAbsolute}
+            src={
+              'https://res.cloudinary.com/dletnd7ha/video/upload/v1689270498/kanako-site/voyeur_aa5wgm.mp4'
+            }
+            muted
+            autoPlay
+            loop
+            width={1200}
+            height={800}
+          ></video>
+          <Overlay />
+          <Canvas />
+        </div>
+      )}
     </article>
   );
 }
---
+++ src/components/Overlay.js
@@ -0,0 +1,36 @@
+import styles from '../app/styles/home.module.css';
+import Image from 'next/image';
+export default function Overlay() {
+  return (
+    <>
+      <Image
+        className={styles.imageOne}
+        src="https://res.cloudinary.com/dletnd7ha/image/upload/v1689270478/kanako-site/topp_vziosf.png"
+        alt="abstract-shape"
+        width={1200}
+        height={600}
+      ></Image>
+      <Image
+        className={styles.imageTwo}
+        src="https://res.cloudinary.com/dletnd7ha/image/upload/v1689270444/kanako-site/_middle.png"
+        alt="abstract-shape"
+        width={1200}
+        height={600}
+      ></Image>
+      <Image
+        className={styles.imageThree}
+        src="https://res.cloudinary.com/dletnd7ha/image/upload/v1689270443/kanako-site/_bottom_wru6bd.png"
+        alt="abstract-shape"
+        width={1200}
+        height={600}
+      ></Image>
+      <Image
+        className={styles.imageFour}
+        src="/bottom_circle.png"
+        alt="abstract-shape"
+        width={1200}
+        height={600}
+      ></Image>
+    </>
+  );
+}
